418e843f21cce45797d6dceec90ffb33b39b60fb
Akhil Mangla <akhil_mangla@epam.com>
2024-11-07 18:52:30 +0530

    change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Welcome to the Platform teesri bar!"})
+    return jsonify({"message": "Welcome to the Platform 4 bar!"})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
